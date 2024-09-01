1ca736ee691a3747cf63fafcc05564f8004d0bbe
Ace10079 <minions10karan@gmail.com>
2024-09-01 15:20:38 +0530

    first committ

--- Models/userResponse.js
+++ Models/userResponse.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
 
 const userResponseSchema = new mongoose.Schema({
-  name: String,
-  phoneNumber: String,
-  responses: [String],
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  complaintType: {
+    type: String,
+    enum: ['Individual', 'Association', 'Company'],
+    required: false
+  },
+  department: {
+    type: String,
+    enum: ['PWD', 'Electrical', 'Health'],
+    required: false
+  },
+  responses: {
+    type: [String], // Array to store any additional responses
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const UserResponse = mongoose.model('UserResponse', userResponseSchema);
